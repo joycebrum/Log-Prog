397da3789900b045958eb93c9dba88c9accfb278
Joyce <joycebrumu.u@gmail.com>
2019-03-25 22:49:30 -0300

    parser parcialmente funcionando
    exemplo com bug
    
    (a v b v (d v e v(a v g)))

--- trabalho1.py
+++ trabalho1.py
@@ -11,89 +11,65 @@ funcao = ""
 
 def valor(f, v):
     global funcao
-    print(f)
     string2 = f
     funcao=f
     if(f) :
-        formula = parse3(0)
+        formula = parse(0)
         print(formula)
-        # if formula 
+        return execute(formula,v)
     else :
-        return;
+        return "erro, entrada inválida";
 
 
-def parse(funcaoR):
+def parse(pos):
     global funcao
     global contador
     parseado = []
-    for c in range(0,len(funcaoR)):
-        
-        print("c= ", funcaoR[c])
-        print("funcao = ", funcao)
-        print("função2 = ", funcaoR)
-        print("parseado = ", parseado)
-        print("")
-        if funcaoR[c] == '(':
-            subarray = parse(funcaoR[c+1:len(funcaoR)])
-            pos=0
-            for d in  range(c,len(funcaoR)):
-                if(funcaoR[d] == ')'):
-                   pos=d
-                   break
-
-            print("*********************************")
-            print(funcaoR[c:pos+1])
-            print("*"+str(contador))
-            print("***********************************")
-            funcao.replace(funcaoR[c:pos+1],"abc")
-            print(funcao)
-            contador = contador+1
-            parseado.append(subarray)
-        elif funcaoR[c] == ')':
-            return parseado
-            
-        else:
-            parseado.append(funcaoR[c])
-    return parseado
-
-
-def parse0(func, positionPai):
-    global contador
-    global funcao
-    tamanhoString = len(func)
-    
-    for i in range( tamanhoString ):
-        if(i>=tamanhoString):
-            break
-        if(funcao[i] == '(') :
-            posicao = parse(funcao[i+1:len(func)], i) + i
-            subString = funcao[i:posicao+1]
-            mapa.append(subString)
-            funcao = funcao[0:positionPai] + "*" + str(contador) + funcao[posicao+1:len(funcao)]
-            contador=contador+1
-            print(funcao)
-            tamanhoString = len(funcao)
-        elif(funcao[i] == ')') :
-             return i +1
-    return len(funcao)
-
-def parse3(pos):
-    global funcao
-    global contador
-    parseado = []
-     
-    for i in range( pos, len(funcao)):
-        print("C= ", funcao[i])
-        print("funcao= ", funcao)
-        if(funcao[i] == '('):
-            subarray = parse3(i+1)
+    i = pos
+    #for i in range( pos, len(funcao)):
+    while i < len(funcao):
+        print("C = " + funcao[i])
+        print(funcao)
+        if funcao[i] == '*':
+            while funcao[i] != ')':
+                i = i + 1
+            i = i + 1
+            print("pulou para o char " + funcao[i])
+        elif(funcao[i] == '('):
+            subarray = parse(i+1)
             parseado.append(subarray)
         elif(funcao[i] == ')'):
-            f=funcao
-            f.replace(funcao[pos:i], "*" + str(contador))
-            print("********************")
-            print(f)
+            funcao = funcao.replace(funcao[pos:i], "*" + str(contador))
             return parseado
-        else:
+        elif funcao[i] == '-' and funcao[i+1] == '>':
+            parseado.append("->")
+            i = i + 1
+        elif funcao[i] != ' ':
             parseado.append(funcao[i])
+        i = i + 1
     return parseado
+
+def execute(formula, v):
+    if isinstance(formula, list):
+        i = 0
+        while i < len(formula) :
+            
+            if formula[i] == '^' or formula[i] == 'v':
+                valorEsquerdo = execute(formula[i-1], v)
+                valorDireito = execute(formula[i+1], v)
+                formula.remove(formula[i-1])
+                formula.remove(formula[i+1])
+                if element == '^':
+                    formula[i] = valorEsquerdo and valorDireito
+                else:
+                    formula[i] = valorEsquerdo or valorDireito
+
+            i = i + 1
+    elif isinstance(formula, str):
+        return v[formula]
+    elif isinstance(formula, bool):
+        return formula
+    else:
+        print("erro, formato invalido")
+        return false
+            
